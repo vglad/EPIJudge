644b63d6d627ebf0ee7cfd45a5b6a19ae31eb436
Volodymyr Gladun <vgladun@gmail.com>
2019-07-24 00:45:24 +0100

    solve problem 9.1 ("Is tree balanced")

--- epi_judge_cpp/is_tree_balanced.cc
+++ epi_judge_cpp/is_tree_balanced.cc
@@ -1,8 +1,23 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
+
+int height(BinaryTreeNode<int>* node) {
+    if (node == nullptr) {
+        return 0;
+    }
+    auto l = height(node->left.get());
+    auto r = height(node->right.get());
+    return (l > r ? l : r) + 1;
+}
+
 bool IsBalanced(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return true;
+    if (tree == nullptr) {
+        return true;
+    }
+    int lHeight, rHeight;
+    lHeight = height(tree->left.get());
+    rHeight = height(tree->right.get());
+    return abs(lHeight - rHeight) <= 1 && IsBalanced(tree->left) && IsBalanced(tree->right);
 }
 
 int main(int argc, char* argv[]) {
