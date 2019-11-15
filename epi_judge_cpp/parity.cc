65125c555a1016a87aaf4282ffd2f68867a21be7
Volodymyr Gladun <vgladun@gmail.com>
2019-11-15 20:13:12 +0000

    added several solution to problem 5.1 "Compiting the parity of a word"

--- epi_judge_cpp/parity.cc
+++ epi_judge_cpp/parity.cc
@@ -1,7 +1,30 @@
 #include "test_framework/generic_test.h"
 short Parity(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
+
+    //brut-force O(n)
+//    uint8_t num_bits {0};
+//    while (x) {
+//      num_bits += x & 1;
+//      x >>= 1;
+//    }
+//    return num_bits & 1;
+
+    //O(k), where k number of set bits in x
+//    uint8_t result {0};
+//    while (x) {
+//        result ^= 1u;
+//        x &= (x-1);       //clears the lowest set bit of x
+//    }
+//    return result;
+
+    //O(log n), the XOR of a group of bits is its parity
+    x ^= x >> 32u;
+    x ^= x >> 16u;
+    x ^= x >> 8u;
+    x ^= x >> 4u;
+    x ^= x >> 2u;
+    x ^= x >> 1u;
+    return x & 1;
 }
 
 int main(int argc, char* argv[]) {
