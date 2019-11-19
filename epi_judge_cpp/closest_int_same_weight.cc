a170a4815c5cda099c51a88bb391bbc426a6eadf
Volodymyr Gladun <vgladun@gmail.com>
2019-11-19 17:34:42 +0000

    added draft for problem 4.4 "Find closest integer with the same weight"

--- epi_judge_cpp/closest_int_same_weight.cc
+++ epi_judge_cpp/closest_int_same_weight.cc
@@ -1,7 +1,16 @@
 #include "test_framework/generic_test.h"
 unsigned long long ClosestIntSameBitCount(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
+    uint64_t y = static_cast<uint64_t>(x);
+    u_int64_t compare_bit_mask = 3ULL; //0b11
+    u_int64_t final_bit_mask = compare_bit_mask;
+    while (y) {
+        if ((y & compare_bit_mask) == 1) return x ^= final_bit_mask;
+        if ((y & compare_bit_mask) == 2) return x ^= final_bit_mask;
+        y <<= 1ULL;
+        final_bit_mask <<= 1ULL;
+    }
+
+  return x;
 }
 
 int main(int argc, char* argv[]) {
--- epi_judge_cpp/reverse_bits.cc
+++ epi_judge_cpp/reverse_bits.cc
@@ -1,14 +1,29 @@
 #include "test_framework/generic_test.h"
-unsigned long long ReverseBits(unsigned long long x) {
-    uint64_t result {0};
-    for (auto i = 0; i < 64; ++i) {
-        if (x & 1) {
-            uint64_t y = 1ULL << (63 - i);
-            result |= y;
-        }
-        x >>= 1;
+
+long long swap_bits(long long x, int i, int j) {
+    if (((x >> i) & 1) != ((x >> j) & 1)) {
+        const auto bit_mask = (1ULL << i) | (1ULL << j);
+        x ^= bit_mask;
     }
-    return result;
+    return x;
+}
+unsigned long long ReverseBits(unsigned long long x) {
+//Solution A, copying bits to new variable
+//    uint64_t result {0};
+//    for (auto i = 0; i <= 63; ++i) {
+//        if (x & 1) {
+//            result |= 1ULL << (63 - i);
+//        }
+//        x >>= 1;
+//    }
+//    return result;
+
+//Solution B, using swap_bits function
+    for (uint8_t i = 0; i < 32; ++i) {
+        x = swap_bits(x, i, 63 - i);
+    }
+    return x;
+//Solution C, can be done by lookup in array of reverse 16bit ints
 }
 
 int main(int argc, char* argv[]) {
