60f7c267b4f07a4585c3e4754b1c8dadd062fc85
Volodymyr Gladun <vgladun@gmail.com>
2019-11-20 17:52:40 +0000

    added several solutions to problem 4.4 "Find closest integer with the same weight"

--- epi_judge_cpp/closest_int_same_weight.cc
+++ epi_judge_cpp/closest_int_same_weight.cc
@@ -1,16 +1,37 @@
 #include "test_framework/generic_test.h"
 unsigned long long ClosestIntSameBitCount(unsigned long long x) {
-    uint64_t y = static_cast<uint64_t>(x);
-    u_int64_t compare_bit_mask = 3ULL; //0b11
-    u_int64_t final_bit_mask = compare_bit_mask;
+// Solution A
+//    uint64_t y = x;
+//    const uint64_t compare_bit_mask = 0b11;
+//    uint64_t final_bit_mask = compare_bit_mask;
+//    while (y) {
+//        const uint64_t compare_result = y & compare_bit_mask;
+//        if (compare_result == 1 || compare_result == 2) {
+//            return x ^ final_bit_mask;
+//        }
+//        y >>= 1ULL;
+//        final_bit_mask <<= 1ULL;
+//    }
+
+//Solution B (refactored A)
+    uint64_t y = x, bit_mask = 0b11;
     while (y) {
-        if ((y & compare_bit_mask) == 1) return x ^= final_bit_mask;
-        if ((y & compare_bit_mask) == 2) return x ^= final_bit_mask;
-        y <<= 1ULL;
-        final_bit_mask <<= 1ULL;
+        switch (y & 0b11) {
+            case 1 ... 2 : return x ^ bit_mask;  //0b01 or 0b10
+            default: { y >>= 1ULL; bit_mask <<= 1ULL; }
+        }
     }
 
-  return x;
+//Solution C
+//    const uint8_t num_bits = 64;
+//    for (uint8_t i = 0; i < num_bits - 1; ++i) {
+//        if (((x >> i) & 1) != ((x >> (i + 1)) & 1)) { //if nearest bits not equal
+//            x ^= (1ULL << i) | (1ULL << (i + 1));     //swap bits
+//            return x;
+//        }
+//    }
+
+    throw std::invalid_argument("All bits of x are 0 or 1");
 }
 
 int main(int argc, char* argv[]) {
