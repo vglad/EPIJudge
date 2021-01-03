6d3b55588785e9b0fd3fab75225219c68a96aa5b
Volodymyr Gladun <volodymyr.gladun@doxim.com>
2021-01-03 00:26:37 +0000

    Array 5.5: Delete duplicates from a sorted array (sorted_array_remove_dups.cc)

--- epi_judge_cpp/sorted_array_remove_dups.cc
+++ epi_judge_cpp/sorted_array_remove_dups.cc
@@ -3,11 +3,24 @@
 #include "test_framework/generic_test.h"
 #include "test_framework/timed_executor.h"
 using std::vector;
+
 // Returns the number of valid entries after deletion.
 int DeleteDuplicates(vector<int>* A_ptr) {
-  // TODO - you fill in here.
-  return 0;
+	auto &A = *A_ptr;
+	if (A.empty()) return 0;
+
+	auto curIdx = 1;
+	for (int i = 1; i < A.size(); ++i)
+	{
+		if (A.at(curIdx - 1) != A.at(i)) {
+			A.at(curIdx) = A.at(i);
+			++curIdx;
+		}
+	}
+
+	return curIdx;
 }
+
 vector<int> DeleteDuplicatesWrapper(TimedExecutor& executor, vector<int> A) {
   int end = executor.Run([&] { return DeleteDuplicates(&A); });
   A.resize(end);
