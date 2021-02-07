96a5b137dab61940511948a2a01c68fe45c7f317
Volodymyr Gladun <20119531+vglad@users.noreply.github.com>
2021-02-07 20:12:08 +0000

    Array 5.9: Enumerate all primes to n (prime_sieve.cc)

--- epi_judge_cpp/prime_sieve.cc
+++ epi_judge_cpp/prime_sieve.cc
@@ -4,8 +4,34 @@
 using std::vector;
 // Given n, return all primes up to and including n.
 vector<int> GeneratePrimes(int n) {
-  // TODO - you fill in here.
-  return {};
+	if (n <= 1) return {};
+	if (n == 2) return {2};
+
+	std::vector<int> primes{2};
+
+	size_t number = 3;
+	while (number <= n) {
+		bool is_prime = true;
+
+		if (number % 2 == 0) {
+			is_prime = false;
+		}
+		else {
+			const size_t boundary = std::sqrt(number);
+			for (size_t i = 3; i <= boundary; i += 2) {
+				if (number % i == 0) {
+					is_prime = false;
+					break;
+				}
+			}
+		}
+
+		if (is_prime) primes.emplace_back(number);
+
+		++number;
+	}
+
+  return primes;
 }
 
 int main(int argc, char* argv[]) {
